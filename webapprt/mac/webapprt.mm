397ad242ecf8324090d3d77eacc23dba68ded329
Dan Walkowski <dan.walkowski@gmail.com>
2012-03-15 16:20:46 -0700

    cleanup, use compiled in version number, no longer write to info.plist (still read)

--- webapprt/mac/webapprt.mm
+++ webapprt/mac/webapprt.mm
@@ -38,7 +38,7 @@
 
 //PLAN:
 
- // open my bundle, get the version number
+ // open my bundle, check for an override binary signature
  // find the newest firefox. open its bundle, get the version number.
  // if the firefox version is different than ours:
  //   delete our own binary, (unlink)
@@ -64,17 +64,16 @@
 #include "nsCOMPtr.h"
 #include "nsILocalFile.h"
 #include "nsStringGlue.h"
-//#include "nsWindowsWMain.cpp"            // we want a wmain entry point
 
 
 
-const char *WEBAPPRT_EXECUTABLE = "webapprt";
-const char *APPINI_NAME = "application.ini";
-const char *WEBRTINI_NAME = "webapprt.ini";
+const char WEBAPPRT_EXECUTABLE[] = "webapprt";
+const char APPINI_NAME[] = "application.ini";
+const char WEBRTINI_NAME[] = "webapprt.ini";
 
 //need the correct relative path here
-const char *WEBAPPRT_PATH = "/Contents/MacOS/"; 
-const char *INFO_FILE_PATH = "/Contents/Info.plist";
+const char WEBAPPRT_PATH[] = "/Contents/MacOS/"; 
+const char INFO_FILE_PATH[] = "/Contents/Info.plist";
 
 void execNewBinary(NSString* launchPath);
 
@@ -84,6 +83,8 @@ NSException* makeException(NSString* name, NSString* message);
 
 void displayErrorAlert(NSString* title, NSString* message);
 
+//this is our version, to be compared with the version of the binary we are asked to use
+NSString* myVersion = [NSString stringWithFormat:@"%s", NS_STRINGIFY(GRE_MILESTONE)];
 
 int gVerbose = 0;
 
@@ -92,7 +93,8 @@ XRE_CreateAppDataType XRE_CreateAppData;
 XRE_FreeAppDataType XRE_FreeAppData;
 XRE_mainType XRE_main;
 
-  const nsDynamicFunctionLoad kXULFuncs[] = {
+  const nsDynamicFunctionLoad kXULFuncs[] = 
+  {
       { "XRE_GetFileFromPath", (NSFuncPtr*) &XRE_GetFileFromPath },
       { "XRE_CreateAppData", (NSFuncPtr*) &XRE_CreateAppData },
       { "XRE_FreeAppData", (NSFuncPtr*) &XRE_FreeAppData },
@@ -100,19 +102,22 @@ XRE_mainType XRE_main;
       { nsnull, nsnull }
   };
 
-  nsresult AttemptGRELoad(char *greDir) {
+  nsresult AttemptGRELoad(char *greDir) 
+  {
     nsresult rv;
     char xpcomDLLPath[MAXPATHLEN];
     snprintf(xpcomDLLPath, MAXPATHLEN, "%s%s", greDir, XPCOM_DLL);
 
     rv = XPCOMGlueStartup(xpcomDLLPath);
 
-    if(NS_FAILED(rv)) {
+    if(NS_FAILED(rv)) 
+    {
       return rv;
     }
 
     rv = XPCOMGlueLoadXULFunctions(kXULFuncs);
-    if(NS_FAILED(rv)) {
+    if(NS_FAILED(rv)) 
+    {
       return rv;
     }
 
@@ -120,12 +125,15 @@ XRE_mainType XRE_main;
   }
 
   // Copied from toolkit/xre/nsAppData.cpp.
-  void SetAllocatedString(const char *&str, const char *newvalue) {
+  void SetAllocatedString(const char *&str, const char *newvalue) 
+  {
     NS_Free(const_cast<char*>(str));
-    if (newvalue) {
+    if (newvalue) 
+    {
       str = NS_strdup(newvalue);
     }
-    else {
+    else 
+    {
       str = nsnull;
     }
   }
@@ -139,39 +147,37 @@ int main(int argc, char **argv)
 
   for (i=1;i < argc;i++)
   {
-    if (!strcmp(argv[i], "-v")) {
+    if (!strcmp(argv[i], "-v")) 
+    {
       gVerbose = 1;
-    } else if (!strcmp(argv[i], "-b")) {
-      if (i+1 < argc) {
+    } 
+    else if (!strcmp(argv[i], "-b")) 
+    {
+      if (i+1 < argc) 
+      {
         alternateBinaryID = [NSString stringWithFormat: @"%s", argv[i+1]];
         i++;
       }
     }
   }
 
+  NSLog(@"MY WEBAPPRT VERSION: %@", myVersion);
 
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];  
 
   //I need to look in our bundle first, before deciding what firefox binary to use
   NSString* myBundlePath = [[NSBundle mainBundle] bundlePath];
   NSString* myInfoFilePath = [NSString stringWithFormat:@"%@%s", myBundlePath, INFO_FILE_PATH];
-  if (![[NSFileManager defaultManager] fileExistsAtPath:myInfoFilePath]) {
+  if (![[NSFileManager defaultManager] fileExistsAtPath:myInfoFilePath]) 
+  {
     //get out of here, I don't have a bundle file?
     NSLog(@"webapp bundle file not found at path: %@", myInfoFilePath);
     @throw makeException(@"Invalid App Package", @"This App appears to be damaged, please reinstall it");
   }
-  //get writable copy of my Info.plist file as a dictionary
-  NSData *myAppData = [[NSData alloc] initWithContentsOfFile:myInfoFilePath];
 
-  NSError *errorDesc = nil;
-  NSMutableDictionary* myAppInfo = 
-          (NSMutableDictionary *)[NSPropertyListSerialization propertyListWithData: myAppData 
-                                                              options: NSPropertyListMutableContainersAndLeaves
-                                                              format: NULL 
-                                                              error: &errorDesc];
-  if (errorDesc != nil) {
-    //we failed to deserialize the property list, so we likely need to bail
-    NSLog(@"unable to deserialize webapp property list: %@", errorDesc);
+  NSDictionary *myAppInfo = [NSDictionary dictionaryWithContentsOfFile:myInfoFilePath];
+  if (!myAppInfo) 
+  {
     @throw makeException(@"Unable To Read App Version", @"This App appears to be damaged, please reinstall it");
   }
 
@@ -179,112 +185,106 @@ int main(int argc, char **argv)
   alternateBinaryID = [myAppInfo objectForKey:@"FirefoxBinary"];
   NSLog(@"found override firefox binary: %@", alternateBinaryID);
 
-  //ok now read my version string
-  NSString* myVersion = [myAppInfo objectForKey:@"CFBundleVersion"];
-  NSLog(@"WebappRT version found: %@", myVersion);
-  [myAppData release];
-  //Finished getting my current version
+  @try 
+  {
 
-  @try {
-  if (!firefoxPath) {
-    firefoxPath = pathToWebRT(alternateBinaryID); // specify an alternate firefox to use in the Info.plist file
-    if (!firefoxPath) {
-      // Launch a dialog to explain to the user that there's no compatible web runtime
-      NSLog(@"unable to find a valid webrt parth");
-      @throw makeException(@"Missing Web Runtime", @"Web Applications require Firefox to be installed");
-    }
-  }  
+    if (!firefoxPath) 
+    {
+      firefoxPath = pathToWebRT(alternateBinaryID); // specify an alternate firefox to use in the Info.plist file
+      if (!firefoxPath) 
+      {
+        // Launch a dialog to explain to the user that there's no compatible web runtime
+        NSLog(@"unable to find a valid webrt path");
+        @throw makeException(@"Missing Web Runtime", @"Web Applications require Firefox to be installed");
+      }
+    }  
 
 
-  //Get the version number from my Info.plist.  This is more complicated that you might think; the information isn't meant for us,
-  // it's meant for the OS.  We're supposed to know our version number.  I am reading the file and parsing into a mutable
-  // dictionary object so that we can update it if necessary with the current version number and write it out again.
-
-  NSString *myWebRTPath = [NSString stringWithFormat: @"%@%s%s", myBundlePath, WEBAPPRT_PATH, WEBAPPRT_EXECUTABLE];
-  NSLog(@"my webrt path: %@", myWebRTPath);
-  if (![[NSFileManager defaultManager] fileExistsAtPath:myWebRTPath]) {
-    @throw makeException(@"Missing WebRT Files", @"Cannot locate binary for this application");
-  }
-
-  //Check to see if the DYLD_FALLBACK_LIBRARY_PATH is set. if not, set it, and relaunch ourselves
-  char libEnv[MAXPATHLEN];
-  snprintf(libEnv, MAXPATHLEN, "%s%s", [firefoxPath UTF8String], WEBAPPRT_PATH);
-
-  char* curVal = getenv("DYLD_FALLBACK_LIBRARY_PATH");
-  NSLog(@"libenv: %s", libEnv);
-  NSLog(@"curval: %s", curVal);
-
-  if ((curVal == NULL) || strncmp(libEnv, curVal, MAXPATHLEN)) {
-    NSLog(@"DYLD_FALLBACK_LIBRARY_PATH NOT SET!!");
-    //they differ, so set it and relaunch
-    setenv("DYLD_FALLBACK_LIBRARY_PATH", libEnv, 1);
-    execNewBinary(myWebRTPath);
-    exit(0);
-  }
-  NSLog(@"Set DYLD_FALLBACK_LIBRARY_PATH to: %s", libEnv);
-
-
-  //now get Firefox version, but read-only, which is much simpler.
-  NSString* firefoxInfoFile = [NSString stringWithFormat:@"%@%s", firefoxPath, INFO_FILE_PATH];
-  if (![[NSFileManager defaultManager] fileExistsAtPath:firefoxInfoFile]) {
-    //get out of here, I don't have a bundle file?
-    NSLog(@"Firefox bundle file not found at path: %@", firefoxInfoFile);
-    @throw makeException(@"Missing Application File", @"This copy of Firefox appears to be damaged (unable to locate Info.plist)");
-  }
-
-  //get read-only copy of FF Info.plist file
-  NSDictionary *firefoxAppInfo = [NSDictionary dictionaryWithContentsOfFile:firefoxInfoFile];
-  if (!firefoxAppInfo) {
-    @throw makeException(@"Unreadable Info File", @"This copy of Firefox appears to be damaged (Info.plist Unreadable)");
-  }
-  NSString* firefoxVersion = [firefoxAppInfo objectForKey:@"CFBundleVersion"];
-  //Finished getting Firefox version
-
-
-  //compare them
-  if ([myVersion compare: firefoxVersion] != NSOrderedSame) {
-    //we are going to assume that if they are different, we need to re-copy the webapprt, regardless of whether
-    // it is newer or older.  If we don't find a webapprt, then the current Firefox must not be new enough to run webapps.
-    NSLog(@"This Application has an old webrt. Updating it.");
-
-    //we know the firefox path, so copy the new webapprt here
-    NSString *newWebRTPath = [NSString stringWithFormat: @"%@%s%s", firefoxPath, WEBAPPRT_PATH, WEBAPPRT_EXECUTABLE];
-    NSLog(@"firefox webrt path: %@", newWebRTPath);
-    if (![[NSFileManager defaultManager] fileExistsAtPath:newWebRTPath]) {
-      NSString* msg = [NSString stringWithFormat: @"This version of Firefox (%@) cannot run web applications, because it is not recent enough or damaged", firefoxVersion];
-      @throw makeException(@"Missing WebRT Files", msg);
+    NSString *myWebRTPath = [NSString stringWithFormat: @"%@%s%s", myBundlePath, WEBAPPRT_PATH, WEBAPPRT_EXECUTABLE];
+    NSLog(@"my webrt path: %@", myWebRTPath);
+    if (![[NSFileManager defaultManager] fileExistsAtPath:myWebRTPath]) 
+    {
+      @throw makeException(@"Missing WebRT Files", @"Cannot locate binary for this application");
     }
 
-        //unlink my binary file
-    int err = unlink([myWebRTPath UTF8String]);
-    if (err) {
-      NSLog(@"failed to unlink old binary file at path: %@", myWebRTPath);
-      @throw makeException(@"Unable To Update", @"Failed preparation for runtime update");
+    //Check to see if the DYLD_FALLBACK_LIBRARY_PATH is set. if not, set it, and relaunch ourselves
+    char libEnv[MAXPATHLEN];
+    snprintf(libEnv, MAXPATHLEN, "%s%s", [firefoxPath UTF8String], WEBAPPRT_PATH);
+
+    char* curVal = getenv("DYLD_FALLBACK_LIBRARY_PATH");
+    //NSLog(@"libenv: %s", libEnv);
+    //NSLog(@"curval: %s", curVal);
+
+    if ((curVal == NULL) || strncmp(libEnv, curVal, MAXPATHLEN)) 
+    {
+      NSLog(@"DYLD_FALLBACK_LIBRARY_PATH NOT SET!!");
+      //they differ, so set it and relaunch
+      setenv("DYLD_FALLBACK_LIBRARY_PATH", libEnv, 1);
+      execNewBinary(myWebRTPath);
+      exit(0);
+    }
+    //NSLog(@"Set DYLD_FALLBACK_LIBRARY_PATH to: %s", libEnv);
+
+
+    //now get Firefox version, but read-only, which is much simpler.
+    NSString* firefoxInfoFile = [NSString stringWithFormat:@"%@%s", firefoxPath, INFO_FILE_PATH];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:firefoxInfoFile]) 
+    {
+      //get out of here, I don't have a bundle file?
+      NSLog(@"Firefox bundle file not found at path: %@", firefoxInfoFile);
+      @throw makeException(@"Missing Application File", @"This copy of Firefox appears to be damaged (unable to locate Info.plist)");
     }
 
-    NSFileManager* clerk = [[NSFileManager alloc] init];
-    [clerk copyItemAtPath: newWebRTPath toPath: myWebRTPath error: &errorDesc];
-    [clerk release];
-    if (errorDesc != nil) {
-      NSLog(@"failed to copy new webrt file");
-      @throw makeException(@"Unable To Update", @"Failed to update runtime");
+    //get read-only copy of FF Info.plist file
+    NSDictionary *firefoxAppInfo = [NSDictionary dictionaryWithContentsOfFile:firefoxInfoFile];
+    if (!firefoxAppInfo) 
+    {
+      @throw makeException(@"Unreadable Info File", @"This copy of Firefox appears to be damaged (Info.plist Unreadable)");
     }
+    NSString* firefoxVersion = [firefoxAppInfo objectForKey:@"CFBundleVersion"];
+    //Finished getting Firefox version
 
-    //If we successfully copied over the newer webapprt, then overwrite my version number with the firefox version number,
-    // and save the dictionary out as my new Info.plist
-    [myAppInfo setObject: firefoxVersion forKey: @"CFBundleVersion"];
 
-    NSData* newProps = [NSPropertyListSerialization dataWithPropertyList:myAppInfo format: NSPropertyListXMLFormat_v1_0 options:0 error: &errorDesc];
-    BOOL success = [newProps writeToFile: myInfoFilePath atomically: YES];
-    if (!success) {
-      NSLog(@"Failed to write out updated Info.plist: %@", errorDesc);
-      @throw makeException(@"Version Update Failure", @"Failed to update the app version number to the current version");
-    }
+    //compare them
+    if ([myVersion compare: firefoxVersion] != NSOrderedSame) 
+    {
+      //we are going to assume that if they are different, we need to re-copy the webapprt, regardless of whether
+      // it is newer or older.  If we don't find a webapprt, then the current Firefox must not be new enough to run webapps.
+      NSLog(@"This Application has an old webrt. Updating it.");
+
+      //we know the firefox path, so copy the new webapprt here
+      NSString *newWebRTPath = [NSString stringWithFormat: @"%@%s%s", firefoxPath, WEBAPPRT_PATH, WEBAPPRT_EXECUTABLE];
+      NSLog(@"firefox webrt path: %@", newWebRTPath);
+      if (![[NSFileManager defaultManager] fileExistsAtPath:newWebRTPath]) 
+      {
+        NSString* msg = [NSString stringWithFormat: @"This version of Firefox (%@) cannot run web applications, because it is not recent enough or damaged", firefoxVersion];
+        @throw makeException(@"Missing WebRT Files", msg);
+      }
+
+          //unlink my binary file
+      int err = unlink([myWebRTPath UTF8String]);
+      if (err) 
+      {
+        NSLog(@"failed to unlink old binary file at path: %@", myWebRTPath);
+        @throw makeException(@"Unable To Update", @"Failed preparation for runtime update");
+      }
+
+      NSError *errorDesc = nil;
+      NSFileManager* clerk = [[NSFileManager alloc] init];
+      [clerk copyItemAtPath: newWebRTPath toPath: myWebRTPath error: &errorDesc];
+      [clerk release];
+      if (errorDesc != nil) 
+      {
+        NSLog(@"failed to copy new webrt file");
+        @throw makeException(@"Unable To Update", @"Failed to update runtime");
+      }
 
     //execv the new binary, and ride off into the sunset
     execNewBinary(myWebRTPath);
 
-  } else {
+  } 
+  else 
+  {
     //we are ready to load XUL and such, and go go go
 
       NSLog(@"This Application has the newest webrt.  Launching!");
@@ -296,7 +296,8 @@ int main(int argc, char **argv)
       // Set up our environment to know where application.ini was loaded from.
       char appEnv[MAXPATHLEN];
       snprintf(appEnv, MAXPATHLEN, "%s%s%s", [myBundlePath UTF8String], WEBAPPRT_PATH, APPINI_NAME);
-      if (setenv("XUL_APP_FILE", appEnv, 1)) {
+      if (setenv("XUL_APP_FILE", appEnv, 1)) 
+      {
         NSLog(@"Couldn't set XUL_APP_FILE to: %s", appEnv);
         return 255;
       }
@@ -310,7 +311,8 @@ int main(int argc, char **argv)
       isGreLoaded = NS_SUCCEEDED(AttemptGRELoad(greDir));
 
 
-      if(!isGreLoaded) {
+      if(!isGreLoaded) 
+      {
         // TODO: User-friendly message explaining that FF needs to be installed
         return 255;
       }
@@ -319,7 +321,8 @@ int main(int argc, char **argv)
 
       { // Scope for any XPCOM stuff we create
           nsINIParser parser;
-          if(NS_FAILED(parser.Init(appEnv))) {
+          if(NS_FAILED(parser.Init(appEnv))) 
+          {
             NSLog(@"%s was not found\n", appEnv);
             return 255;
           }
@@ -332,37 +335,43 @@ int main(int argc, char **argv)
 
         // Load the runtime's INI from its path.
         nsCOMPtr<nsILocalFile> rtINI;
-        if(NS_FAILED(XRE_GetFileFromPath(rtINIPath, getter_AddRefs(rtINI)))) {
+        if(NS_FAILED(XRE_GetFileFromPath(rtINIPath, getter_AddRefs(rtINI)))) 
+        {
           NSLog(@"Runtime INI path not recognized: '%s'\n", rtINIPath);
           return 255;
         }
 
-        if(!rtINI) {
+        if(!rtINI) 
+        {
           NSLog(@"Error: missing webapprt.ini");
           return 255;
         }
 
         nsXREAppData *webShellAppData;
-        if (NS_FAILED(XRE_CreateAppData(rtINI, &webShellAppData))) {
+        if (NS_FAILED(XRE_CreateAppData(rtINI, &webShellAppData))) 
+        {
           NSLog(@"Couldn't read webapprt.ini: %s", rtINIPath);
           return 255;
         }
 
         char profile[MAXPATHLEN];
-        if(NS_FAILED(parser.GetString("App", "Profile", profile, MAXPATHLEN))) {
+        if(NS_FAILED(parser.GetString("App", "Profile", profile, MAXPATHLEN))) 
+        {
           NSLog(@"Unable to retrieve profile from web app INI file");
           return 255;
         }
         SetAllocatedString(webShellAppData->profile, profile);
 
         nsCOMPtr<nsILocalFile> directory;
-        if(NS_FAILED(XRE_GetFileFromPath(greDir, getter_AddRefs(directory)))) {
+        if(NS_FAILED(XRE_GetFileFromPath(greDir, getter_AddRefs(directory)))) 
+        {
           NSLog(@"Unable to open app dir");
           return 255;
         }
 
         nsCOMPtr<nsILocalFile> xreDir;
-        if(NS_FAILED(XRE_GetFileFromPath(greDir, getter_AddRefs(xreDir)))) {
+        if(NS_FAILED(XRE_GetFileFromPath(greDir, getter_AddRefs(xreDir)))) 
+        {
           NSLog(@"Unable to open XRE dir");
           return 255;
         }
@@ -383,11 +392,13 @@ int main(int argc, char **argv)
   }
   
 }
-@catch (NSException *e) {
+@catch (NSException *e) 
+{
   NSLog(@"got exception: %@", e);
   displayErrorAlert([e name], [e reason]);
 }
-@finally {
+@finally 
+{
   [pool drain];
   exit(1);
 }
@@ -395,7 +406,8 @@ int main(int argc, char **argv)
 }
 
 
-NSException* makeException(NSString* name, NSString* message) {
+NSException* makeException(NSString* name, NSString* message) 
+{
   NSException* myException = [NSException
         exceptionWithName:name
         reason:message
@@ -443,9 +455,7 @@ void execNewBinary(NSString* launchPath)
   // char binfile[500];
   // sprintf(binfile, "%s/%s", WEBAPPRT_EXECUTABLE);
 
-  const char *newargv[2];
-  newargv[0] = [launchPath UTF8String];
-  newargv[1] = NULL;
+  const char *const newargv[] = {[launchPath UTF8String], NULL};
 
   NSLog(@"COMMAND LINE: '%@ %s'", launchPath, newargv[0]);
   execv([launchPath UTF8String], (char **)newargv);
