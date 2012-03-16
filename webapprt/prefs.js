a5ff8fe06c14abe5afcd2d6716d626c85bf44618
Myk Melez <myk@mozilla.org>
2012-03-15 23:31:58 -0700

    rename the content package from webapp to webapprt for consistency

--- webapprt/prefs.js
+++ webapprt/prefs.js
@@ -2,5 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("browser.chromeURL", "chrome://webapp/content/window.xul");
-pref("browser.hiddenWindowChromeURL", "chrome://webapp/content/hiddenWindow.xul");
+pref("browser.chromeURL", "chrome://webapprt/content/window.xul");
+pref("browser.hiddenWindowChromeURL", "chrome://webapprt/content/hiddenWindow.xul");
