46c678fb38d7c1ae4b86786b1bdc7ab388426c2e
Myk Melez <myk@mozilla.org>
2012-03-21 16:53:07 -0700

    refactor menuitem updating code to reduce code duplication

--- webapprt/content/webappWindow.js
+++ webapprt/content/webappWindow.js
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-Cu.import("resource://gre/modules/Webapps.jsm");
-Cu.import("resource://gre/modules/WebappRT.jsm");
-
 function onLoad() {
   window.removeEventListener("load", onLoad, false);
 
--- webapprt/content/window.js
+++ webapprt/content/window.js
@@ -8,6 +8,22 @@ const Cu = Components.utils;
 
 Cu.import("resource://gre/modules/Webapps.jsm");
 Cu.import("resource://gre/modules/WebappRT.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+#ifdef XP_MACOSX
+// On Mac, we dynamically create the label for the Quit menuitem, using
+// a string property to inject the name of the webapp into it.
+window.addEventListener("load", function onLoadUpdateQuitMenuItem() {
+  window.removeEventListener("load", onLoadUpdateQuitMenuItem, false);
+  let installRecord = DOMApplicationRegistry.getApp(Webapp.origin);
+  let manifest = DOMApplicationRegistry.getManifestSync(Webapp.origin);
+  let bundle =
+    Services.strings.createBundle("chrome://webapprt/locale/window.properties");
+  let quitLabel = bundle.formatStringFromName("quitApplicationCmdMac.label",
+                                              [manifest.name], 1);
+  document.getElementById("menu_FileQuitItem").setAttribute("label", quitLabel);
+}, false);
+#endif
 
 function updateEditUIVisibility() {
 #ifndef XP_MACOSX
